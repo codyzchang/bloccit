f091b10c7fe22a8d6e8fa360133850345160d281
Cody <cody.z.chang@gmail.com>
2015-03-20 17:14:33 +0000

    auth assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,29 @@
  class PostPolicy < ApplicationPolicy
-   
+   class Scope
+     attr_reader :user, :scope
+
+     def initialize(user, scope)
+       @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if  user.admin? || user.moderator?
+        scope.all
+  #    elsif 
+   #     scope.all
+      elsif nil
+        return "You are not an Admin"
+      
+      else
+        scope.where(:published => true)        
+    end
+    end
+  end
+
+  def update?
+    user.admin? or user.moderator? or not post.published?
+  end
+
    
  end
