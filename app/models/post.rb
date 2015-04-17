775c74779366e8f6d2ab4ae3482c469bb9884718
Cody <cody.z.chang@gmail.com>
2015-04-17 19:01:02 +0000

    voting chkpt

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,8 +11,9 @@ class Post < ActiveRecord::Base
   
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  validates :topic, presence: true
-  validates :user, presence: true
+  
+ # validates :topic, presence: true
+ # validates :user, presence: true
 
   def markdown_title
     render_as_markdown(self.title)
