c69f030b6f65a57f0783a9c6f014ca07222bca24
Cody <cody.z.chang@gmail.com>
2015-04-28 02:13:13 +0000

    added mentor fixes

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,77 @@
+class Topics::PostsController < ApplicationController
+ 
+  def show
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    authorize @post
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
+  end
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    authorize @post
+
+    if @post.save
+      flash.now[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+      flash.now[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    authorize @post
+
+    if @post.update_attributes(post_params)
+      flash.now[:notice] = "Post was updated."
+      redirect_to [@topic, @post]
+    else
+      flash.now[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+  
+  
+  def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     title = @post.title
+     authorize @post
+ 
+     if @post.destroy
+       flash.now[:notice] = "\"#{title}\" was deleted successfully."
+       redirect_to @topic
+     else
+       flash.now[:error] = "There was an error deleting the post."
+       render :show
+     end
+   end
+  
+ private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+  
+  
+  
+  
+  
+end
