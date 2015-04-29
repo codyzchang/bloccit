b0f47b55a6217cc8c05af1e7e77b76b7eaa4d6eb
Cody <cody.z.chang@gmail.com>
2015-04-29 01:39:40 +0000

    publicprofile assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,20 +1,20 @@
- require 'rails_helper'
+ require 'rails_helper' 
+
  
  describe "Visiting profiles" do
  
    include TestFactories
  
    before do
-     @user = authenticated_user
-     @post = associated_post(user: @user)
+    @user = authenticated_user
+    @post = associated_post(user: @user)
     @comment = Comment.new(user: @user, post: @post, body: "A Comment")
     allow(@comment).to receive(:send_favorite_emails)
     @comment.save!
    end
  
    describe "not signed in" do
- 
-     it "shows profile" do
+      it "shows profile" do
        visit user_path(@user)
        expect(current_path).to eq(user_path(@user))
        
@@ -24,4 +24,35 @@
      end
  
    end
+   
+   
+   include Warden::Test::Helpers
+   Warden.test_mode!
+   
+    before do
+    @user = authenticated_user
+    @post = associated_post(user: @user)
+    @comment = Comment.new(user: @user, post: @post, body: "A Comment")
+    allow(@comment).to receive(:send_favorite_emails)
+    @comment.save!
+   end
+   
+   describe "a user visiting their own profile" do
+     it "shows profile" do
+       
+       before login_as(user, :scope => :user)
+       
+       visit user_path(@user)
+       expect(current_path).to eq(user_path(@user))
+   
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+   
+   
+   
+   
+   
+   
+   
  end
