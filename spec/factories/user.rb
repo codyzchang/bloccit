67d5dee275fa9e950548fe82d1a66e4a134a5385
Cody <cody.z.chang@gmail.com>
2015-05-02 02:16:08 +0000

    testing-users assignment

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,20 @@
  FactoryGirl.define do
    factory :user do
+          
      name "Douglas Adams"
      sequence(:email, 100) { |n| "person#{n}@example.com" }
      password "helloworld"
      password_confirmation "helloworld"
      confirmed_at Time.now
-   end
+     
+     
+     
+     factory :user_with_post_and_comment do
+       after(:build)  |user|  do
+         post = create(:post)
+         comment = create (:comment)
+       end
+     end
+       
+     
  end
