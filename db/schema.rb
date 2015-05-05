719ee0c80afe97396f0ea6eaf7b41c5c768ed09f
Cody <cody.z.chang@gmail.com>
2015-05-05 23:27:57 +0000

    fixed image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424153837) do
+ActiveRecord::Schema.define(version: 20150505231728) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150424153837) do
     t.integer  "user_id"
     t.integer  "topic_id"
     t.float    "rank"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
